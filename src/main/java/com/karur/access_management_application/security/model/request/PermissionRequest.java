6208b51b2cca79b1d61e83740095d1c7bd445ac1
Guru Raghavendra Hegde <hegde.r.guru@gmail.com>
2026-07-12 17:27:06 +0530

    update

--- src/main/java/com/karur/access_management_application/security/model/request/PermissionRequest.java
+++ src/main/java/com/karur/access_management_application/security/model/request/PermissionRequest.java
@@ -1,5 +1,6 @@
 package com.karur.access_management_application.security.model.request;
 
+import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PermissionRequest {
+
     private String classPath;
     private String className;
     private String fieldName;
@@ -17,4 +19,26 @@ public class PermissionRequest {
     private boolean create;
     private boolean update;
     private boolean delete;
+    private String fullyQualifiedClassPath;
+    private String fullyQualifiedFieldPath;
+    private boolean[] permissions;
+
+    @PostConstruct
+    public void init() {
+        fullyQualifiedClassPath = fullyQualifiedClassPath();
+        fullyQualifiedFieldPath = fullyQualifiedFieldPath();
+        permissions = permissions();
+    }
+
+    public String fullyQualifiedClassPath() {
+        return classPath + "." + className;
+    }
+
+    public String fullyQualifiedFieldPath() {
+        return fullyQualifiedClassPath() + "." + fieldName;
+    }
+
+    public boolean[] permissions() {
+        return new boolean[]{read, create, update, delete};
+    }
 }
