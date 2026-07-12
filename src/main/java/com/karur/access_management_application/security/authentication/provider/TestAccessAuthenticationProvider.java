aced9799e604e596e2eb57898395d27b41e1d911
Guru Raghavendra Hegde <hegde.r.guru@gmail.com>
2026-07-12 13:13:08 +0530

    update

--- src/main/java/com/karur/access_management_application/security/authentication/provider/TestAccessAuthenticationProvider.java
+++ src/main/java/com/karur/access_management_application/security/authentication/provider/TestAccessAuthenticationProvider.java
@@ -1,8 +1,13 @@
 package com.karur.access_management_application.security.authentication.provider;
 
+import com.karur.access_management_application.security.service.AccessDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -11,6 +16,13 @@ import javax.naming.AuthenticationException;
 @Service
 @Profile({"local", "test"})
 public class TestAccessAuthenticationProvider implements SupportedAuthenticationProvider {
+
+    @Autowired
+    AccessDetailsService accessDetailsService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @Override
     public boolean supports(Class<?> authenticatinClass) {
         return TestingAuthenticationToken.class.isAssignableFrom(authenticatinClass);
@@ -21,8 +33,9 @@ public class TestAccessAuthenticationProvider implements SupportedAuthentication
         if (!supports(authentication.getClass())) {
             Mono.error(new AuthenticationException("Access is not allowed"));
         }
-        TestingAuthenticationToken testingAuthenticationToken = (TestingAuthenticationToken) authentication;
-        testingAuthenticationToken.setAuthenticated(true);
-        return Mono.just(authentication);
+        return accessDetailsService.findByUsername(authentication.getName())
+                .switchIfEmpty(Mono.error(new IllegalAccessException("User not found")))
+                .flatMap(Mono::just)
+                .map(userDetails -> new TestingAuthenticationToken(userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities()));
     }
 }
