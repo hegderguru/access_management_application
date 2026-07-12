5085366147f0adcb41e8600657243bde59f608c4
Guru Raghavendra Hegde <hegde.r.guru@gmail.com>
2026-07-12 12:49:58 +0530

    update

--- src/main/java/com/karur/access_management_application/security/repository/RoleEntityRepository.java
+++ src/main/java/com/karur/access_management_application/security/repository/RoleEntityRepository.java
@@ -10,7 +10,5 @@ import java.util.List;
 
 @Repository
 public interface RoleEntityRepository extends ReactiveCrudRepository<RoleEntity,Long> {
-    Flux<RoleEntity> findByAuthorityId(Long authorityId);
-
     Flux<RoleEntity> findByIdIn(List<Long> ids);
 }
