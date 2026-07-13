60f531e78c76d2649c6044e5c840b152cef112ef
hegderguru <hegde.r.guru@gmail.com>
2026-07-13 21:23:52 +0530

    update

--- src/main/java/com/karur/access_management_application/security/mapper/requestToEntity/AuthorityRequestToEntityMapper.java
+++ src/main/java/com/karur/access_management_application/security/mapper/requestToEntity/AuthorityRequestToEntityMapper.java
@@ -1,20 +1,14 @@
 package com.karur.access_management_application.security.mapper.requestToEntity;
 
 import com.karur.access_management_application.security.entity.AccessAuthorityEntity;
-import com.karur.access_management_application.security.entity.AccessEntity;
 import com.karur.access_management_application.security.entity.AuthorityEntity;
 import com.karur.access_management_application.security.model.request.AuthorityRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class AuthorityRequestToEntityMapper {
-
-    public List<AccessAuthorityEntity> buildAccessAuthorityEntities(AccessEntity accessEntity, List<AuthorityEntity> authorityEntities) {
-        return authorityEntities.stream().map(authorityEntity -> buildAccessAuthorityEntity(accessEntity.getId(), authorityEntity)).toList();
-    }
 
     public AccessAuthorityEntity buildAccessAuthorityEntity(Long accessId, AuthorityEntity authorityEntity) {
         return AccessAuthorityEntity.builder()
--- src/main/java/com/karur/access_management_application/security/mapper/requestToEntity/PermissionRequestToEntityMapper.java
+++ src/main/java/com/karur/access_management_application/security/mapper/requestToEntity/PermissionRequestToEntityMapper.java
@@ -1,13 +1,17 @@
 package com.karur.access_management_application.security.mapper.requestToEntity;
 
 import com.karur.access_management_application.security.entity.PermissionEntity;
-import com.karur.access_management_application.security.entity.RoleEntity;
 import com.karur.access_management_application.security.entity.RolePermissionEntity;
 import com.karur.access_management_application.security.model.request.PermissionRequest;
 
-import java.util.List;
-
 public class PermissionRequestToEntityMapper {
+
+    public RolePermissionEntity buildRolePermissionEntity(Long roleId, PermissionEntity permissionEntity) {
+        return RolePermissionEntity.builder()
+                .roleId(roleId)
+                .permissionId(permissionEntity.getId())
+                .build();
+    }
 
     public PermissionEntity buildPermissionEntity(PermissionRequest permissionRequest) {
         return PermissionEntity.builder()
@@ -18,17 +22,6 @@ public class PermissionRequestToEntityMapper {
                 .create(permissionRequest.getCreate())
                 .update(permissionRequest.getUpdate())
                 .delete(permissionRequest.getDelete())
-                .build();
-    }
-
-    public List<RolePermissionEntity> buildRolePermissionEntities(RoleEntity roleEntity, List<PermissionEntity> permissionEntities) {
-        return permissionEntities.stream().map(permissionEntity -> buildRolePermissionEntity(roleEntity.getId(), permissionEntity)).toList();
-    }
-
-    public RolePermissionEntity buildRolePermissionEntity(Long roleId, PermissionEntity permissionEntity) {
-        return RolePermissionEntity.builder()
-                .roleId(roleId)
-                .permissionId(permissionEntity.getId())
                 .build();
     }
 }
