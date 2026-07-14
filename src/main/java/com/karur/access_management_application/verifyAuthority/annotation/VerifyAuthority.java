39cc1411bf2320ae8b38774d23519677c723d3f9
hegderguru <hegde.r.guru@gmail.com>
2026-07-14 22:02:54 +0530

    update

---
+++ src/main/java/com/karur/access_management_application/verifyAuthority/annotation/VerifyAuthority.java
@@ -0,0 +1,11 @@
+package com.karur.access_management_application.verifyAuthority.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface VerifyAuthority {
+}
