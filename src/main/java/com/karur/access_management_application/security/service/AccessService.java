8e1731fe4d14e1beac94e6750fbea8b30226596f
hegderguru <hegde.r.guru@gmail.com>
2026-07-14 21:47:56 +0530

    update

--- src/main/java/com/karur/access_management_application/security/service/AccessService.java
+++ src/main/java/com/karur/access_management_application/security/service/AccessService.java
@@ -1,23 +1,14 @@
 package com.karur.access_management_application.security.service;
 
-import com.karur.access_management_application.security.compare.CompareUtil;
-import com.karur.access_management_application.security.entity.AccessEntity;
-import com.karur.access_management_application.security.entity.AuthorityEntity;
-import com.karur.access_management_application.security.mapper.entiyToRequest.EntityToAccessReuestMapper;
 import com.karur.access_management_application.security.mapper.entityToRead.EntityToReadMapper;
 import com.karur.access_management_application.security.mapper.requestToEntity.*;
 import com.karur.access_management_application.security.model.read.AccessDetail;
 import com.karur.access_management_application.security.model.request.AccessRequest;
 import com.karur.access_management_application.security.repository.AccessRepository;
-import com.karur.access_management_application.security.util.AccessRequestUpdateUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.jspecify.annotations.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.util.List;
 
 @Slf4j
 @Service
@@ -30,22 +21,7 @@ public class AccessService {
     RequestToEntityMapper requestToEntityMapper;
 
     @Autowired
-    EntityToAccessReuestMapper entityToAccessReuestMapper;
-
-    @Autowired
     EntityToReadMapper entityToReadMapper;
-
-    @Autowired
-    AccessRequestToEntityMapper accessRequestToEntityMapper;
-
-    @Autowired
-    AuthorityRequestToEntityMapper authorityRequestToEntityMapper;
-
-    @Autowired
-    RoleRequestToEntityMapper roleRequestToEntityMapper;
-
-    @Autowired
-    PermissionRequestToEntityMapper permissionRequestToEntityMapper;
 
     public Mono<AccessDetail> fetchAccessDetails(String username) {
         return accessRepository.fetchAccessEntity(username).flatMap(accessEntity -> Mono.just(entityToReadMapper.buildAccessDetail(accessEntity)));
