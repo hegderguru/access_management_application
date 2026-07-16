15fb2e8c2adb113a8bc4764da1ae604bf62c74f3
Guru Raghavendra Hegde <hegde.r.guru@gmail.com>
2026-07-16 16:54:51 +0530

    update

--- src/main/java/com/karur/access_management_application/security/service/AccessService.java
+++ src/main/java/com/karur/access_management_application/security/service/AccessService.java
@@ -3,7 +3,11 @@ package com.karur.access_management_application.security.service;
 import com.karur.access_management_application.security.mapper.entityToRead.EntityToReadMapper;
 import com.karur.access_management_application.security.mapper.requestToEntity.*;
 import com.karur.access_management_application.security.model.read.AccessDetail;
+import com.karur.access_management_application.security.model.read.AuthorityDetail;
+import com.karur.access_management_application.security.model.read.RoleDetail;
 import com.karur.access_management_application.security.model.request.AccessRequest;
+import com.karur.access_management_application.security.model.request.AuthorityRequest;
+import com.karur.access_management_application.security.model.request.RoleRequest;
 import com.karur.access_management_application.security.repository.AccessRepository;
 import com.karur.access_management_application.validate.annotation.ValidateData;
 import lombok.extern.slf4j.Slf4j;
@@ -29,10 +33,22 @@ public class AccessService {
         return accessRepository.fetchAccessEntity(username).flatMap(accessEntity -> Mono.just(entityToReadMapper.buildAccessDetail(accessEntity)));
     }
 
-    public Mono<AccessDetail> create(AccessRequest accessRequest) {
+    public Mono<AccessDetail> createAccess(AccessRequest accessRequest) {
         return Mono.defer(() -> Mono.just(requestToEntityMapper.buildOnlyAccessEntity(accessRequest)))
                 .flatMap(accessEntity -> accessRepository.saveAccessEntity(accessEntity))
                 .map(savedAccessEntity -> entityToReadMapper.buildAccessDetail(savedAccessEntity));
+    }
+
+    public Mono<AuthorityDetail> createAuthority(AuthorityRequest authorityRequest) {
+        return Mono.defer(() -> Mono.just(requestToEntityMapper.buildOnlyAuthorityEntity(authorityRequest)))
+                .flatMap(authorityEntity -> accessRepository.saveAccessEntity(authorityEntity))
+                .map(authorityEntity -> entityToReadMapper.buildAuthorityDetail(authorityEntity));
+    }
+
+    public Mono<RoleDetail> createRole(RoleRequest roleRequest) {
+        return Mono.defer(() -> Mono.just(requestToEntityMapper.buildRoleEntity(roleRequest)))
+                .flatMap(roleEntity -> accessRepository.saveRoleEntity(roleEntity))
+                .map(roleEntity -> entityToReadMapper.buildRoleDetail(roleEntity));
     }
 
     public Mono<AccessDetail> update(AccessRequest accessRequest) {
@@ -40,6 +56,4 @@ public class AccessService {
                 .flatMap(accessEntity -> accessRepository.saveAccessEntity(accessEntity))
                 .map(savedAccessEntity -> entityToReadMapper.buildAccessDetail(savedAccessEntity));
     }
-
-
 }
