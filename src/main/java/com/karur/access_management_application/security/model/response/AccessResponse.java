067001ede496dbf97fe8d2ead5b696a8340a1492
Guru Raghavendra Hegde <hegde.r.guru@gmail.com>
2026-07-16 17:51:09 +0530

    update

--- src/main/java/com/karur/access_management_application/security/model/response/AccessResponse.java
+++ src/main/java/com/karur/access_management_application/security/model/response/AccessResponse.java
@@ -3,9 +3,15 @@ package com.karur.access_management_application.security.model.response;
 import com.karur.access_management_application.security.model.read.AccessDetail;
 import com.karur.access_management_application.security.model.read.AuthorityDetail;
 import com.karur.access_management_application.security.model.read.RoleDetail;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Data
 public class AccessResponse {
 
