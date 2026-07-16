29b28e729ea4b7bc0d6fabcc98777452624038e7
Guru Raghavendra Hegde <hegde.r.guru@gmail.com>
2026-07-16 18:46:58 +0530

    update

--- src/main/java/com/karur/access_management_application/security/mapper/requestToEntity/PermissionRequestToEntityMapper.java
+++ src/main/java/com/karur/access_management_application/security/mapper/requestToEntity/PermissionRequestToEntityMapper.java
@@ -3,16 +3,13 @@ package com.karur.access_management_application.security.mapper.requestToEntity;
 import com.karur.access_management_application.security.compare.ChangeUtil;
 import com.karur.access_management_application.security.compare.CompareUtil;
 import com.karur.access_management_application.security.entity.PermissionEntity;
-import com.karur.access_management_application.security.entity.RoleEntity;
 import com.karur.access_management_application.security.entity.join.RolePermissionEntity;
 import com.karur.access_management_application.security.model.request.PermissionRequest;
-import com.karur.access_management_application.security.util.AccessRequestUpdateUtil;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
-import java.util.Map;
 
 @Component
 public class PermissionRequestToEntityMapper {
@@ -34,28 +31,6 @@ public class PermissionRequestToEntityMapper {
                 .update(permissionRequest.getUpdate())
                 .delete(permissionRequest.getDelete())
                 .build();
-    }
-
-    public Mono<Void> saveOrUpdatePermissionsOnChanges(RoleEntity roleEntity, List<CompareUtil.Change> changes) {
-        return newPermissionsOnChanges(roleEntity, changes).then(Mono.defer(() -> updatePermissionsOnChanges(roleEntity, changes)));
-    }
-
-    public Mono<Void> newPermissionsOnChanges(RoleEntity roleEntity, List<CompareUtil.Change> changes) {
-        return Flux.fromIterable(AccessRequestUpdateUtil.getNewPermissionRequest(changes))
-                .flatMap(change -> Mono.just(buildPermissionEntity((PermissionRequest) change.getRightValue())))
-                .flatMap(permissionEntity -> {
-                    roleEntity.addPermissionEntity(permissionEntity);
-                    return Mono.empty();
-                }).then();
-    }
-
-    public Mono<Void> updatePermissionsOnChanges(RoleEntity roleEntity, List<CompareUtil.Change> changes) {
-        Map<String, List<CompareUtil.Change>> updateRoleRequest1 = AccessRequestUpdateUtil.getUpdatePermissionRequest(changes);
-        return Flux.fromIterable(updateRoleRequest1.entrySet())
-                .flatMap(stringListEntry -> {
-                    PermissionEntity permissionEntity = roleEntity.getPermissionEntities().stream().filter(authorityEntity1 -> authorityEntity1.fullyQualifiedFieldPath().equalsIgnoreCase(stringListEntry.getKey())).findFirst().get();
-                    return updatePermissionOnChanges(permissionEntity, stringListEntry.getValue());
-                }).then();
     }
 
     private Mono<Void> updatePermissionOnChanges(PermissionEntity permissionEntity, List<CompareUtil.Change> changes) {
